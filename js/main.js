a5da4cd0239a5573c71ab5655ba7a30106753339
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-03-13 09:01:26 +0500

    answer section desktop the end

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+var swiper = new Swiper(".resultSwiper", {
+    breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 5,
+          spaceBetween: 50,
+        },
+    },
+    navigation: {
+      nextEl: ".result-button-next",
+      prevEl: ".result-button-prev",
+    },
+});
+
+
 let media_menu = document.querySelector('.media_menu');
 let medu_close = document.querySelector('.medu_close');
 let media_menu_bg = document.querySelector('.media_menu_bg');
